6c33ae9276d75a3264373a39ca30f986ed6ce2a2
Burak Bayezid <80351492+burakatilgan12@users.noreply.github.com>
2021-03-11 05:26:01 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,51 @@
+muhittin_susamuru = {'Ad': "Muhittin", 'Soyad' : "Susamuru", "Meslek" : "Tesisatçı"}
+nuri_kalburbastı = {'Ad' : "Nuri", 'Soyad' :'Kalburbastı' , 'Meslek' : 'Nalbur'}
+hüseyin_sincanlı = {'Ad' : 'Hüseyin', 'Soyad' : 'Sincanlı', 'Meslek' : 'İşsiz'}
+abidin_aynacı =  {'Ad' : 'Abidin' , 'Soyad' : 'Aynacı', 'Meslek' : 'Kasiyer' }
+gezen_gezmiş = {'Ad' : 'Gezen', 'Soyad' : 'Gezmiş', 'Meslek' : 'Şoför'}
+
+#muhittin_susamuru
+print("Çalışan Ad =", muhittin_susamuru['Ad'])
+print("Çalışan Soyad = ", muhittin_susamuru['Soyad'])
+print("Meslek =" ,muhittin_susamuru['Meslek'])
+
+print("##########")
+#nuri_kalburbastı
+print("Çalışan Ad = ",nuri_kalburbastı['Ad'])
+print("Çalışan Soyad = ", nuri_kalburbastı['Soyad'])
+print("Meslek =", nuri_kalburbastı['Meslek'])
+
+print("##########")
+#hüseyin_sincanlı
+print("Çalışan Ad = ",hüseyin_sincanlı['Ad'])
+print("Çalışan Soyad = ",hüseyin_sincanlı ['Soyad'])
+print("Meslek =",hüseyin_sincanlı ['Meslek'])
+
+print("##########")
+#abidin_aynacı
+print("Çalışan Ad = ",abidin_aynacı['Ad'])
+print("Çalışan Soyad = ", abidin_aynacı['Soyad'])
+print("Meslek =",abidin_aynacı ['Meslek'])
+
+print("##########")
+#gezen_gezmiş
+print("Çalışan Ad = ",gezen_gezmiş['Ad'])
+print("Çalışan Soyad = ", gezen_gezmiş['Soyad'])
+print("Meslek =" ,gezen_gezmiş['Meslek'])
+
+print(""""\         _nnnn_
+        dGGGGMMb
+       @p~qp~~qMb
+       M|@||@) M|
+       @,----.JM|
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    `.       | `' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMP|   .'
+     `-'       `--' hjm""")
